7e6e909e07493f676126c9271b0a94ed11808a91
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-02-02 10:54:03 +0100

    Creation des methode

---
+++ CTigerDemon.py
@@ -0,0 +1,12 @@
+from librairy.jsonWorkOnline import *
+from librairy.dectectionOS import *
+
+class CTigerDemon :
+    def __init__(self,nameSoft : str,url :str):
+        pass
+
+    def checkUpdate(self):
+        pass
+
+    def getVersionSoft(self):
+        pass
