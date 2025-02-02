c64f34516a3e325405ce66480eda3d1b167ad213
Baptiste Pauchet <63608854+baptistepau@users.noreply.github.com>
2025-02-02 13:57:39 +0100

    Creation du demons

--- CTigerDemon.py
+++ CTigerDemon.py
@@ -3,10 +3,34 @@ from librairy.dectectionOS import *
 
 class CTigerDemon :
     def __init__(self,nameSoft : str,url :str):
-        pass
+        # objet
+        self.__system = OS()
+        depotFile = jsonWorkOnline()
+        # load depots
+        depotFile.loadInternet(url)
+        # Chargement des informations du logiciel
+        self.__dictSofts = depotFile.dictJson()[nameSoft]
 
     def checkUpdate(self):
-        pass
+
+        versionInstalled = self.getVersionSoft()
+
+        if (versionInstalled != "IXXXX-XXX"):
+            versionOnline = self.__dictSofts["version"]
+            if (versionInstalled != versionOnline):
+                return True
+            else:
+                return False
 
     def getVersionSoft(self):
-        pass
+        versionInstalled = ""
+        with open("VERSION", "r") as fichier:
+            for ligne in fichier:
+                # Si la ligne commence par "VERSION="
+                if ligne.startswith("VERSION="):
+                    # Supprimer le saut de ligne éventuel et récupérer la valeur
+                    version = ligne.strip().split("=")[1]
+                    versionInstalled = version
+            fichier.close()
+
+        return versionInstalled
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from CTigerDemon import *
 
 def main():
-    pass
+    tigerDemon = CTigerDemon("ryley","https://arrera-software.fr/depots.json")
+    print(tigerDemon.checkUpdate())
+    print(tigerDemon.getVersionSoft())
 
 if __name__ == "__main__":
     main()
