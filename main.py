bc9a68f6db29d907d27f266c012d7e4fbd034257
Baptiste P <baptiste.pauchet13@gmail.com>
2026-01-17 17:10:00 +0100

    Ajout de texte

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from version_demon import *
 def main():
     conf = soft_config(name_soft="ryley",version="I2025-0.00")
     tigerDemon = demon(conf,"https://arrera-software.fr/depots.json")
-    print(tigerDemon.getInternet())
-    print(tigerDemon.checkUpdate())
-    print(tigerDemon.getVersionSoft())
+    print(f"Etat internet : {tigerDemon.getInternet()}")
+    print(f"Etat Update : {tigerDemon.checkUpdate()}")
+    print(f"Version : {tigerDemon.getVersionSoft()}")
 
 if __name__ == "__main__":
     main()
